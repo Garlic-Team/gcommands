1e3962e90bcf8977f6975d9b2dc31a2a83f5364b
BuildTools <unconfigured@null.spigotmc.org>
2021-04-23 16:54:30 +0200

    inlineReply

--- src/main/GCommands.js
+++ src/main/GCommands.js
@@ -19,7 +19,6 @@ module.exports = class GCommands {
         this.cooldowns = new Collection();
 
         this.cmdDir = options.cmdDir;
-        this.ignoreBots = options.ignoreBots ? options.ignoreBots : false;
 
         this.prefix = options.slash.prefix.toLowerCase() ? options.slash.prefix.toLowerCase() : undefined;
         this.slash = options.slash.slash ? options.slash.slash : true;
@@ -272,120 +271,143 @@ module.exports = class GCommands {
 }
 
 class Message extends Structures.get("Message") {
+    async buttons(content, options) {
+        if (!options.buttons) {
+            options.buttons = [];
+        }
 
-  async buttons(content, options) {
+        if (!Array.isArray(options.buttons)) {
+            return console.log(new Color("&d[GCommands] &cThe buttons must be array.",{json:false}).getText());
+        }
 
-      if (!options.buttons) {
-          options.buttons = [];
-      }
+        let buttons = [];
+        let styles = ['blupurple', 'grey', 'green', 'red', 'url'];
 
-      if (!Array.isArray(options.buttons)) {
-        return console.log(new Color("&d[GCommands] &cThe buttons must be array.",{json:false}).getText());
-      }
+        options.buttons.forEach((x, i) => {
+            if (!x.style) x.style = 'blupurple';
 
-      let buttons = [];
-      let styles = ['blupurple', 'grey', 'green', 'red', 'url'];
-
-      options.buttons.forEach((x, i) => {
-          if (!x.style) x.style = 'blupurple';
-
-          if (!styles.includes(x.style)) {
-            return console.log(new Color(`&d[GCommands] &c#${i} button has invalid style, recived ${x.style}`,{json:false}).getText());
-          }
-
-          if (!x.label) {
-            return console.log(new Color(`&d[GCommands] &c#${i} don't has label!`,{json:false}).getText());
-          }
-
-          if (typeof (x.label) !== 'string') x.label = String(x.label);
-
-          if (x.style === 'url') {
-            if (!x.url) {
-                return console.log(new Color(`&d[GCommands] &cIf the button style is "url", you must provide url`,{json:false}).getText());
+            if (!styles.includes(x.style)) {
+                return console.log(new Color(`&d[GCommands] &c#${i} button has invalid style, recived ${x.style}`,{json:false}).getText());
             }
-          } else {
-            if (!x.id) {
-                return console.log(new Color(`&d[GCommands] &cIf the button style is not "url", you must provide custom id`,{json:false}).getText());
+
+            if (!x.label) {
+                return console.log(new Color(`&d[GCommands] &c#${i} don't has label!`,{json:false}).getText());
             }
-           }
 
-          var style;
+            if (typeof (x.label) !== 'string') x.label = String(x.label);
 
-          if (x.style === 'blupurple') {
-              style = 1;
-          } else if (x.style === 'grey') {
-              style = 2;
-          } else if (x.style === 'green') {
-              style = 3;
-          } else if (x.style === 'red') {
-              style = 4;
-          } else if (x.style === 'url') {
-              style = 5;
-          }
+            if (x.style === 'url') {
+                if (!x.url) {
+                    return console.log(new Color(`&d[GCommands] &cIf the button style is "url", you must provide url`,{json:false}).getText());
+                }
+            } else {
+                if (!x.id) {
+                    return console.log(new Color(`&d[GCommands] &cIf the button style is not "url", you must provide custom id`,{json:false}).getText());
+                }
+            }
 
-          let data = {
-              type: 2,
-              style: style,
-              label: x.label,
-              custom_id: x.id || null,
-              url: x.url || null
-          }
+            var style;
 
-          buttons.push(data);
-      })
+            if (x.style === 'blupurple') {
+                style = 1;
+            } else if (x.style === 'grey') {
+                style = 2;
+            } else if (x.style === 'green') {
+                style = 3;
+            } else if (x.style === 'red') {
+                style = 4;
+            } else if (x.style === 'url') {
+                style = 5;
+            }
 
-      options.buttons === null;
+            let data = {
+                type: 2,
+                style: style,
+                label: x.label,
+                custom_id: x.id || null,
+                url: x.url || null
+            }
 
-      this.client.ws.on('INTERACTION_CREATE', async (data) => {
-          let typeStyles = {
-              1: 'blupurple',
-              2: 'grey',
-              3: 'green',
-              4: 'red',
-              5: 'url'
-          };
+            buttons.push(data);
+        })
 
-          await this.client.channels.cache.get(data.channel_id).messages.fetch();
+        options.buttons === null;
 
-          var message;
-          try {
-            message = await this.client.channels.cache.get(data.channel_id).messages.cache.get(data.message.id);
-          } catch(e) {
-            message = await this.client.channels.cache.get(data.channel_id)
-          }
+        this.client.ws.on('INTERACTION_CREATE', async (data) => {
+            let typeStyles = {
+                1: 'blupurple',
+                2: 'grey',
+                3: 'green',
+                4: 'red',
+                5: 'url'
+            };
 
-          var clicker = await this.client.guilds.cache.get(data.guild_id).members.cache.get(data.member.user.id);
+            await this.client.channels.cache.get(data.channel_id).messages.fetch();
 
-          this.client.emit('clickButton', {
-              version: data.version,
-              type: data.type,
-              style: typeStyles[data.type],
-              token: data.token,
-              id: data.data.custom_id,
-              discordId: data.id,
-              applicationId: data.application_id,
-              clicker: clicker,
-              message
-          })
-      });
+            var message;
+            try {
+                message = await this.client.channels.cache.get(data.channel_id).messages.cache.get(data.message.id);
+            } catch(e) {
+                message = await this.client.channels.cache.get(data.channel_id)
+            }
 
-      this.client.api.channels[this.channel.id].messages.post({
-          headers: {
-              "Content-Type": 'applications/json'
-          },
-          data: {
-              content: content,
-              components: [
-                  {
-                      type: 1,
-                      components: buttons
-                  }
-              ],
-              options,
-              embed: options.embed || null
-          }
-      });
-  }
+            var clicker = await this.client.guilds.cache.get(data.guild_id).members.cache.get(data.member.user.id);
+
+            this.client.emit('clickButton', {
+                version: data.version,
+                type: data.type,
+                style: typeStyles[data.type],
+                token: data.token,
+                id: data.data.custom_id,
+                discordId: data.id,
+                applicationId: data.application_id,
+                clicker: clicker,
+                message
+            })
+        });
+
+        this.client.api.channels[this.channel.id].messages.post({
+            headers: {
+            "Content-Type": 'applications/json'
+            },
+            data: {
+                content: content,
+                components: [
+                    {
+                        type: 1,
+                        components: buttons
+                    }
+                ],
+                options,
+                embed: options.embed || null
+            }
+        });
+    }
+
+    async inlineReply(content, options) {
+        const mentionRepliedUser = typeof ((options || content || {}).allowedMentions || {}).repliedUser === "undefined" ? true : ((options || content).allowedMentions).repliedUser;
+        delete ((options || content || {}).allowedMentions || {}).repliedUser;
+
+        const apiMessage = content instanceof APIMessage ? content.resolveData() : APIMessage.create(this.channel, content, options).resolveData();
+        Object.assign(apiMessage.data, { message_reference: { message_id: this.id } });
+
+        if (!apiMessage.data.allowed_mentions || Object.keys(apiMessage.data.allowed_mentions).length === 0)
+        apiMessage.data.allowed_mentions = { parse: ["users", "roles", "everyone"] };
+        if (typeof apiMessage.data.allowed_mentions.replied_user === "undefined")
+        Object.assign(apiMessage.data.allowed_mentions, { replied_user: mentionRepliedUser });
+
+        if (Array.isArray(apiMessage.data.content)) {
+            return Promise.all(apiMessage.split().map(x => {
+                x.data.allowed_mentions = apiMessage.data.allowed_mentions;
+                return x;
+            }).map(this.inlineReply.bind(this)));
+        }
+
+        const { data, files } = await apiMessage.resolveFiles();
+        return this.client.api.channels[this.channel.id].messages
+            .post({ data, files })
+            .then(d => this.client.actions.MessageCreate.handle(d).message);
+    }
 }
 
 Structures.extend("Message", () => Message);
--- test.js
+++ test.js
@@ -6,7 +6,6 @@ const client = new Discord.Client();
 client.on("ready", () => {
     new GCommands(client, {
         cmdDir: "commands",
-        ignoreBots: true,
         errorMessage: "Error :(",
         slash: {
            slash: 'both', //true = slash only, false = only normal, both = slash and normal
