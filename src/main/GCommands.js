9e059df61f05546477697264f4a3dd2a584b52af
BuildTools <unconfigured@null.spigotmc.org>
2021-04-27 14:02:22 +0200

    Added buttonsWithReply | test 5 (log)

--- src/main/GCommands.js
+++ src/main/GCommands.js
@@ -508,7 +508,7 @@ class MessageStructure extends Structures.get("Message") {
             })
         });
 
-        console.log(this)
+        console.log(this.channel)
         this.client.api.channels[this.channel.id].messages.post({
             headers: {
             "Content-Type": 'applications/json'
