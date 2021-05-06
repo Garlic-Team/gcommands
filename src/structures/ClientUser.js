7931aabf6fffd771ae14d292633d3c7e54a96703
BuildTools <unconfigured@null.spigotmc.org>
2021-05-06 16:16:54 +0200

    Structures | inlineReply fix

---
+++ src/structures/ClientUser.js
@@ -0,0 +1,41 @@
+const { User } = require('discord.js');
+
+module.exports = ClientUser => class extends User {
+    async setGuildPrefix(prefix, guildId) {
+        if(!this.client.database.working) return;
+        if(this.client.database.type = "mongodb") {
+            var guildSettings = require('../models/guild')
+
+            const settings = await guildSettings.findOne({ id: guildId})
+            if(!settings) {
+              await guildSettings.create({
+                id: guildId,
+                prefix: prefix
+              })
+              return;
+            }
+
+            settings.prefix = prefix
+            await settings.save()
+        } else {
+            this.client.database.sqlite.set(`guildPrefix_${guildId}`,`prefix`)
+        }
+    }
+
+    async getGuildPrefix(guildId) {
+        if(!this.client.database.working) return;
+        if(this.client.database.type = "mongodb") {
+            var guildSettings = require('../models/guild')
+
+            const settings = await guildSettings.findOne({ id: guildId})
+            if(!settings) {
+              return this.client.prefix
+            }
+
+            return settings.prefix ? settings.prefix : this.client.prefix
+        } else {
+            var settings = this.client.database.sqlite.get(`guildPrefix_${guildId}`)
+            return settings ? settings : this.client.prefix;
+        }
+    }
+}
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const { GCommands } = require("./src/index");
+const axios = require("axios")
 
 client.on("ready", () => {
     new GCommands(client, {
