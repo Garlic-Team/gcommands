1abab84585c41754ddff0afc9af31b6f2899d282
BuildTools <unconfigured@null.spigotmc.org>
2021-05-16 20:04:03 +0200

    gDebug creating slash

--- src/main/GCommands.js
+++ src/main/GCommands.js
@@ -174,11 +174,11 @@ module.exports = class GCommands {
      * @private
      */
     async __createCommands() {
-        var po = await this.__getAllCommands();
-
+        this.client.emit("gDebug", new Color("&d[GCommands] &3Creating slash commands...").getText())
         let keys = Array.from(this.client.commands.keys());
 
         keys.forEach(async (cmdname) => {
+            this.client.emit("gDebug", new Color("&d[GCommands] &3Creating slash command (&e"+cmdname+"&3)").getText())
             var options = [];
             var subCommandGroup = {};
             var subCommand = [];
@@ -426,7 +426,6 @@ module.exports = class GCommands {
 
             console.log(new Color("&d[GCommands TIP] &3Are guild commands not deleted when you delete them? Use this site for remove &ehttps://gcommands-slash-gui.netlify.app/").getText())
         } catch(e) {
-            this.client.emit("gDebug", new Color("&d[GCommands Debug] &3"+e).getText())
             this.client.emit("gDebug", new Color("&d[GCommands Debug] &3Can't remove commands!").getText())
             
             if((this.client.slash) || (this.client.slash == "both")) {
