8cbd89f6bd08a14d991fee59384f32a05ae6fc2d
xhyrom <generalkubo@gmail.com>
2021-05-29 14:29:48 +0200

    removed anime, ping

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: "ping",
-	description: "Check bot ping",
-	cooldown: 3,
-	run: async({client, respond}) => {
-		respond("My ping is " + Math.round(client.ws.ping) + "ms")
-	}
-};
