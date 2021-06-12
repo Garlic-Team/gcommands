7d4ba78ff406d481fe307980abaf2e5ba0b412a0
xhyrom <generalkubo@gmail.com>
2021-06-12 14:45:13 +0200

    embeds

--- src/structures/DMChannel.js
+++ src/structures/DMChannel.js
@@ -19,7 +19,7 @@ module.exports = Structures.extend("DMChannel", DMChannel => {
 
             if(typeof result != "object") data.content = result;
             if(typeof result == "object" && typeof result.content != "object") data.content = result.content;
-            if(typeof result == "object" && result.content == "object") data.embeds = [result.content];
+            if(typeof result == "object" && typeof result.content == "object") data.embeds = [result.content];
             if(typeof result == "object" && result.allowedMentions) { data.allowedMentions = result.allowedMentions } else data.allowedMentions = { parse: [], repliedUser: true }
             if(typeof result == "object" && result.ephemeral) { data.flags = 64 }
             if(typeof result == "object" && result.components) {
