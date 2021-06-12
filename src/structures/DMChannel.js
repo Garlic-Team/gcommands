d1c177acf31534d71e3e97857032af6466415d4a
xhyrom <generalkubo@gmail.com>
2021-06-12 13:57:37 +0200

    TextChannel#send embed fix

--- src/structures/DMChannel.js
+++ src/structures/DMChannel.js
@@ -13,17 +13,6 @@ module.exports = Structures.extend("DMChannel", DMChannel => {
                 content: result
             }
 
-            if (typeof result === 'object') {
-                if(typeof result == "object" && !result.content) {
-                    const embed = new MessageEmbed(result)
-                    data = await createAPIMessage(interaction, embed)
-                }
-                else if(typeof result.content == "object" ) {
-                    const embed = new MessageEmbed(result.content)
-                    data = await createAPIMessage(interaction, embed)
-                } else data = { content: result.content }
-            }
-
             if(typeof result == "object" && result.allowedMentions) { data.allowedMentions = result.allowedMentions } else data.allowedMentions = { parse: [], repliedUser: true }
             if(typeof result == "object" && result.ephemeral) { data.flags = 64 }
             if(typeof result == "object" && result.components) {
