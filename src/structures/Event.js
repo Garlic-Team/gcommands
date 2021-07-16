2c21d6a7fb03654f0e1ed3277513f42a35579bec
xhyrom <generalkubo@gmail.com>
2021-07-16 20:06:57 +0200

    Event docs fix

--- src/structures/Event.js
+++ src/structures/Event.js
@@ -10,25 +10,25 @@ class Event extends null {}
  * Name
  * @type {String}
  */
-this.name = GEvents.Event.name;
+Event.name = GEvents.Event.name;
 
 /**
  * once
  * @type {Boolean}
  */
-this.once = GEvents.Event.once;
+Event.once = GEvents.Event.once;
 
 /**
  * ws
  * @type {Boolean}
  */
-this.ws = GEvents.Event.ws;
+Event.ws = GEvents.Event.ws;
 
 /**
  * run function
  * @param {Client} 
  * @param {String}
  */
-this.run = GEvents.Event.run
+Event.run = GEvents.Event.run;
 
 module.exports = Event;
