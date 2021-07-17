67a8a27176d68f4d31b09aadb8ca6643f8dcca80
xhyrom <generalkubo@gmail.com>
2021-07-17 18:11:26 +0200

    Arguments 100% working, fixed 1 bug

--- src/commands/types/string.js
+++ src/commands/types/string.js
@@ -14,7 +14,7 @@ class StringArgumentType extends ArgumentType {
 
 	validate(argument, message) {
 		if(argument.choices && !argument.choices.some(ch => ch.value == message.content.toLowerCase())) {
-			return `Please enter one of the following options: ${argument.oneOf.map(opt => `\`${opt.name}\``).join(', ')}`;
+			return `Please enter one of the following options: ${argument.choices.map(opt => `\`${opt.name}\``).join(', ')}`;
 		}
 	}
 }
