e9b13982b7569c77a2d8efe36e5cb7e0844af93d
xhyrom <generalkubo@gmail.com>
2021-07-18 14:22:45 +0200

    cleanup

--- src/structures/TextChannel.js
+++ src/structures/TextChannel.js
@@ -1,6 +1,3 @@
 const { TextChannel } = require('discord.js');
-const ButtonCollectorV12 = require('../structures/v12/ButtonCollector'), ButtonCollectorV13 = require('../structures/v13/ButtonCollector'), SelectMenuCollectorV12 = require('../structures/v12/SelectMenuCollector'), SelectMenuCollectorV13 = require('../structures/v13/SelectMenuCollector')
-const GPayload = require('./GPayload');
-const ifDjsV13 = (require('../util/updater')).checkDjsVersion('13');
 
 module.exports = Object.defineProperties(TextChannel.prototype, require("./TextChannelBase"));
