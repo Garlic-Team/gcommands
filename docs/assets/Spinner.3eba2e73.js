d7dbc9e99696a7b85cf30a7728e5abbc53ee0be5
HyRo <generalkubo@gmail.com>
2021-07-26 12:31:36 +0200

    Add files via upload

---
+++ docs/assets/Spinner.3eba2e73.js
@@ -0,0 +1,1 @@
+import{o as s,b as e,e as l}from"./vendor.1d4532d8.js";const r={},o={class:"relative h-10 w-10 my-60 mx-auto"},a=l("div",{class:"cube1 bg-discord-blurple-560 h-4 w-4 absolute"},null,-1),d=l("div",{class:"cube2 bg-discord-blurple-560 h-4 w-4 absolute"},null,-1);r.render=function(l,r){return s(),e("div",o,[a,d])};export{r as _};
