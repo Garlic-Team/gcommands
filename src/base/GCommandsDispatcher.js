0367d2d08631993c8e44edc088318f8ed04a9b40
codefactor-io <support@codefactor.io>
2021-08-07 09:59:59 +0000

    [CodeFactor] Apply fixes to commit bfb26b2

--- src/base/GCommandsDispatcher.js
+++ src/base/GCommandsDispatcher.js
@@ -198,7 +198,7 @@ class GCommandsDispatcher {
         if (!ifDjsV13) this.application = await this.client.fetchApplication();
         else this.application = await this.client.application.fetch();
 
-        console.log(this.application)
+        console.log(this.application);
         if (this.application.owner === null) this.application.owners = [];
 
         if (this.application.owner instanceof Team) {
