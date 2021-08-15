4a2a5d84fdbcd58f8d9170586ea4c3ee0ed3685d
codefactor-io <support@codefactor.io>
2021-08-15 12:06:04 +0000

    [CodeFactor] Apply fixes to commit 5f672e4

--- src/structures/GError.js
+++ src/structures/GError.js
@@ -2,7 +2,7 @@ const Color = require('./Color');
 
 class GError extends Error {
     constructor(name, message) {
-        super(message)
+        super(message);
         this.message = new Color(`&c${message}`).getText();
         this.name = new Color(`&a${name}`).getText();
     }
