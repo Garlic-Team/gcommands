0813a63c49b3476d541cd7dd98a4997e470acb68
xhyrom <generalkubo@gmail.com>
2021-08-21 09:04:02 +0200

    remove console.log

--- src/structures/GMessage.js
+++ src/structures/GMessage.js
@@ -146,7 +146,6 @@ class GMessage {
                     }
 
                     if ('thread' in data && data.thread !== null) {
-                        console.log(data.thread);
                         ifDjsV13 ? this.client.channels._add(data.thread, this.guild) : this.client.channels.add(data.thread, this.guild);
                     }
 
