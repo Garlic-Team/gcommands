1dadd77fd6f98a5e75d5da87df2341efd9b0a085
codefactor-io <support@codefactor.io>
2021-10-30 08:41:15 +0000

    [CodeFactor] Apply fixes

--- dist/util/util.js
+++ dist/util/util.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.getSomething=getSomething;function getSomething(){return null}
+"use strict"; Object.defineProperty(exports,"__esModule",{ value: true }); exports.getSomething = getSomething; function getSomething() { return null; }
