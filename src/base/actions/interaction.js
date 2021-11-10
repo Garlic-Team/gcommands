7b83c0dee527bc57682564137b722ecd41fe2f2c
S222em <sem.roeten@gmail.com>
2021-11-10 08:32:30 +0100

    Fix clickButton/selectMenu

--- src/base/actions/interaction.js
+++ src/base/actions/interaction.js
@@ -1,10 +1,10 @@
 module.exports = client => {
     client.on('interactionCreate', interaction => {
         if (interaction.isButton()) {
-            client.emit('clickButton', [interaction]);
+            client.emit('clickButton', interaction);
         }
         if (interaction.isSelectMenu()) {
-            client.emit('selectMenu', [interaction]);
+            client.emit('selectMenu', interaction);
         }
     });
 };
