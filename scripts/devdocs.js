11dd42db5d9d5fac54ce08dd17943e2d3aa68d8c
xhyrom <generalkubo@gmail.com>
2021-12-11 22:43:41 +0100

    rm

--- scripts/devdocs.js
+++
@@ -1,6 +0,0 @@
-import {runGenerator} from '@discordjs/ts-docgen';
-
-runGenerator({
-	existingOutput: 'docs/out-dev.json',
-	output: 'docs/dev.json'
-});
--- scripts/docs.js
+++
@@ -1,6 +0,0 @@
-import {runGenerator} from '@discordjs/ts-docgen';
-
-runGenerator({
-	existingOutput: 'docs/out-next.json',
-	output: 'docs/next.json'
-});
