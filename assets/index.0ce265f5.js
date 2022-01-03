7227437a92245a957ba3a042e98575b98f917414
Hyro <generalkubo@gmail.com>
2022-01-03 20:38:35 +0100

    Delete assets directory

--- assets/index.0ce265f5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./Spinner.3eba2e73.js";import{d as o,o as r,b as s}from"./vendor.1d4532d8.js";var a=o({expose:[],setup:o=>(o,a)=>(r(),s(e))});export default a;
--- assets/index.b4ec57b6.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./Spinner.8494312d.js";import{d as o,o as r,b as s}from"./vendor.e3d02f40.js";var a=o({expose:[],setup:o=>(o,a)=>(r(),s(e))});export default a;
