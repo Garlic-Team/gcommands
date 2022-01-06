44c1c7dcbdcb83b4d9353b8a616ab0fa42e43988
xhyrom <generalkubo@gmail.com>
2022-01-06 19:38:52 +0100

    inhibitor fix

--- src/inhibitors/UserRoles.ts
+++ src/inhibitors/UserRoles.ts
@@ -14,14 +14,16 @@ export class UserRoles extends Inhibitor {
 
 	constructor(options: UserRolesOptions) {
 		super(options);
+
 		this.ids = options.ids;
+		this.requireAll = options.requireAll;
 	}
 
 	run(ctx: CommandContext | ComponentContext): boolean | any {
 		if (!ctx.inCachedGuild()) return;
 
 		if (!ctx.member.roles.cache[this.requireAll ? 'hasAll' : 'hasAny'](...this.ids)) return ctx.reply({ 
-			content: 'You do not have the required roles to execute this command',
+			content: this.resolveMessage(ctx) || 'You do not have the required roles to execute this command',
 			ephemeral: this.ephemeral,
 		});
 		else return true;
