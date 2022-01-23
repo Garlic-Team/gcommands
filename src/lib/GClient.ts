a64665dbdda18a46b644fc0080b28efa3880caeb
xhyrom <generalkubo@gmail.com>
2022-01-23 10:32:05 +0100

    fix: remove declare

--- src/lib/GClient.ts
+++ src/lib/GClient.ts
@@ -24,7 +24,7 @@ export interface GClientOptions extends ClientOptions {
 
 export class GClient<Ready extends boolean = boolean> extends Client<Ready> {
 	public responses: Record<string, string> = Responses;
-	public declare options: GClientOptions;
+	public options: GClientOptions;
 
 	constructor(options: GClientOptions) {
 		super(options);
