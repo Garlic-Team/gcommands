11a5b1034b910ee37c8cf70f31fdf25f7798e422
xhyrom <generalkubo@gmail.com>
2022-02-09 19:59:50 +0100

    Revert "Revert "Revert "feat(MessageCommandHandler): arguments support"""
    This reverts commit dd60474281f3c9320f0315b4ca180bf916904ed0.

--- src/lib/structures/arguments/Channel.ts
+++
@@ -1,7 +0,0 @@
-import { MessageArgumentTypeBase } from './base';
-
-export class ChannelType extends MessageArgumentTypeBase {
-	validate() {
-		return true;
-	}
-}
--- src/lib/structures/arguments/Mentionable.ts
+++
@@ -1,7 +0,0 @@
-import { MessageArgumentTypeBase } from './base';
-
-export class MentionableType extends MessageArgumentTypeBase {
-	validate() {
-		return true;
-	}
-}
