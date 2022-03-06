a353ea37c7488f50678e4b679b8e6052b45d5ce5
Hyro <generalkubo@gmail.com>
2022-03-06 20:24:39 +0100

    Update fixer.mjs

--- scripts/fixer.mjs
+++ scripts/fixer.mjs
@@ -6,8 +6,8 @@ let content = readFileSync(path, 'utf-8');
 
 content = content
     .replace(
-        'const file = await Promise.resolve().then(() => (0, tslib_1.__importStar)(require(path.join(dir, rawFileName))));',
-        'const file = await Promise.resolve().then(async() => (0, tslib_1.__importStar)(await import(`file://${path.join(dir, rawFileName)}`)));'
+        'const file = await Promise.resolve().then(() => tslib_1.__importStar(require(path.join(dir, rawFileName))));',
+        'const file = await Promise.resolve().then(async() => tslib_1.__importStar(await import(`file://${path.join(dir, rawFileName)}`)));'
     )
 
 writeFileSync('./dist/lib/loaders/directoryLoader.js', content);
