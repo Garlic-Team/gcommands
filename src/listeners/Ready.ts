02ef557602ed0e41039f8721eabc887603f01b87
S222em <sem.roeten@gmail.com>
2022-03-18 19:25:07 +0100

    Change all `as GClient` to `client.container` since why not

--- src/listeners/Ready.ts
+++ src/listeners/Ready.ts
@@ -1,14 +1,13 @@
 import { Listener } from '../lib/structures/Listener';
 import type { GClient } from '../lib/GClient';
-import type { Client } from 'discord.js';
 import { sync } from '../lib/util/sync';
 import { Logger } from '../lib/util/logger/Logger';
 
 new Listener({
 	event: 'ready',
 	name: 'gcommands-ready',
-	run: async (client: Client<true>) => {
+	run: async (client: GClient<true>) => {
 		Logger.info('Client is ready with %s guild(s)', client.guilds.cache.size);
-		await sync(client as GClient);
+		await sync(client);
 	},
 });
