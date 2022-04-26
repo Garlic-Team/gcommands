827f4b800fbd04ea4f38c2cd2126024c10b9a38a
S222em <sem.roeten@gmail.com>
2022-04-26 12:44:04 +0200

    remove plugin validation (will be reverted)

--- src/lib/structures/Plugin.ts
+++ src/lib/structures/Plugin.ts
@@ -1,7 +1,5 @@
 import type { GClient } from '../GClient';
 import { Plugins } from '../managers/PluginManager';
-import { Logger } from '../util/logger/Logger';
-import { z } from 'zod';
 
 export interface PluginOptions {
 	name: string;
@@ -10,15 +8,6 @@ export interface PluginOptions {
 	afterLogin?: (client: GClient) => any;
 }
 
-const validationSchema = z
-	.object({
-		name: z.string(),
-		afterInitialization: z.function().optional(),
-		beforeLogin: z.function().optional(),
-		afterLogin: z.function().optional(),
-	})
-	.passthrough();
-
 export class Plugin {
 	public name: string;
 	public afterInitialization: (client: GClient) => any;
@@ -26,19 +15,11 @@ export class Plugin {
 	public afterLogin: (client: GClient) => any;
 
 	public constructor(options: PluginOptions) {
-		validationSchema
-			.parseAsync({ ...options, ...this })
-			.then((options) => {
-				this.name = options.name;
-				this.afterInitialization = options.afterInitialization;
-				this.beforeLogin = options.beforeLogin;
-				this.afterLogin = options.afterLogin;
+		this.name = options.name;
+		this.afterInitialization = options.afterInitialization;
+		this.beforeLogin = options.beforeLogin;
+		this.afterLogin = options.afterLogin;
 
-				Plugins.register(this);
-			})
-			.catch((error) => {
-				Logger.warn(typeof error.code !== 'undefined' ? error.code : '', error.message);
-				if (error.stack) Logger.trace(error.stack);
-			});
+		Plugins.register(this);
 	}
 }
