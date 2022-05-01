3f247f343ca6afa241ec8fc9387005ccf694a097
Hyro <generalkubo@gmail.com>
2022-05-01 20:07:17 +0200

    docs: update jsdocs

--- src/lib/util/regexes.ts
+++ src/lib/util/regexes.ts
@@ -30,6 +30,6 @@ export const mentionableRegexp = /^(?:<@!?)?(?:<@&?)?([0-9]+)>?$/;
 
 /**
  * Regex for command and option names
- * @raw `/^[\P{Lu}\p{N}_-]+$/u`
+ * @raw `/^[a-zA-Z1-9]/`
  */
 export const commandAndOptionNameRegexp = /^[a-zA-Z1-9]/;
