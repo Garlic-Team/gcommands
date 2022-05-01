2b5922f796970698c85219e3dcc45176d3f1cc59
Šebestíček <44745014+SebestikCZ@users.noreply.github.com>
2022-05-01 19:42:05 +0200

    feat(Component): edited options option

--- src/lib/structures/Component.ts
+++ src/lib/structures/Component.ts
@@ -91,7 +91,7 @@ export class Component {
 				this.run = options.run || Component.defaults?.run;
 				this.onError = options.onError || Component.defaults?.onError;
 				this.autoDefer = options.autoDefer || Component.defaults?.autoDefer;
-				this.options = this;
+				this.options = options;
 
 				Components.register(this);
 			})
