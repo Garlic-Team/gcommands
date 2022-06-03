a437ba33fc3c52a33f391a13a9dccccad4312748
xhyrom <generalkubo@gmail.com>
2022-06-03 14:36:21 +0200

    docs: some additions

--- src/util/customId.ts
+++ src/util/customId.ts
@@ -1,5 +1,11 @@
 import { uid } from './uid';
 
-export function customId(name: string, ...args: Array<string | number>) {
+/**
+ * The method that generates custom id.
+ * @param {String} name The name of the customId.
+ * @param {string[] | number[]} args The arguments to pass to the customId.
+ * @returns
+ */
+export function customId(name: string, ...args: string[] | number[]) {
 	return `${name}${args[0] ? `-${args.join('-')}` : ''}-${uid()}`;
 }
